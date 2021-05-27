d466a81fb7c386e5fad7d2af8dd3efb9bb8c9f3d
venkat <venkat.201998@gmail.com>
2021-05-27 12:05:01 +0530

    27-05 12:04

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,19 +1,19 @@
 const User = require("../models/user");
 
-exports.createOrUpdateUser = async (req, res) => {
-    console.log("Type of user: ", req.body.type);
-    try{
+exports.createOrUpdateUser = async(req, res) => {
+    try {
+        const type = req.body.type;
         const { email, name, picture } = req.user;
-        const user = await User.findOneAndUpdate({email},{firstName: email.split('@')[0], picture, type}, {new: true});
-        if(user){
+        const user = await User.findOneAndUpdate({ email }, { firstName: email.split('@')[0], picture, type }, { new: true });
+        if (user) {
             console.log("User Updated");
             res.json(user);
-        }else{
-            const newUser = await new User({email: email, firstName: email.split('@')[0], picture, type}).save();
+        } else {
+            const newUser = await new User({ email: email, firstName: email.split('@')[0], picture, type }).save();
             console.log("User Created");
             res.json(newUser);
         }
-    }catch(error){
+    } catch (error) {
         res.json(error);
     }
-};  
+};
