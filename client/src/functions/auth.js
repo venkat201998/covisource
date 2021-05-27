579902bf2e1d8b1a79006bf1f6acd5542fea3516
Priyanka1015 <madhupriyanka1015@gmail.com>
2021-05-27 16:01:23 +0530

    commit

--- client/src/functions/auth.js
+++ client/src/functions/auth.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 export const createOrUpdateUser = async (idToken, type) => {
     return await axios.post(
-        // `${process.env.REACT_APP_API}/create-or-update-user`,
-        "http://localhost:8000/api/create-or-update-user",
+        `${process.env.REACT_APP_API}/create-or-update-user`,
+        // "http://localhost:8000/api/create-or-update-user",
         {
             type
         },
@@ -18,8 +18,8 @@ export const createOrUpdateUser = async (idToken, type) => {
 export const createOrUpdateHospitalUser = async (idToken, type) => {
     console.log("auth.js/functions: ", idToken, type);
     return await axios.post(
-        // `${process.env.REACT_APP_API}/create-or-update-hospital-user`,
-        "http://localhost:8000/api/create-or-update-hospital-user",
+        `${process.env.REACT_APP_API}/create-or-update-hospital-user`,
+        // "http://localhost:8000/api/create-or-update-hospital-user",
         {
             type
         },
