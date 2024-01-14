7a52a329a88aff7d5abfee914eb25a7823be6e39
venkat201998 <84058119+venkat201998@users.noreply.github.com>
2024-01-14 10:55:05 -0500

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,12 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFSPy6Kf3ZpvS4Ca97XSdLGlaOs4aBLHQ",
-  authDomain: "covisource-d675f.firebaseapp.com",
-  projectId: "covisource-d675f",
-  storageBucket: "covisource-d675f.appspot.com",
-  messagingSenderId: "389632412146",
-  appId: "1:389632412146:web:ba6a78d977d5f2322f836b"
+  //place your config here
 };
 
 // Initialize Firebase
