1a81cb79e68eef654caffaf2df2002fae2b1cadf
Venkatanarasimha Reddy Burri <venkat@s-MacBook-Pro.lan>
2024-01-14 12:14:34 -0500

    fixed env key

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -8,7 +8,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_APIKEY,
   authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
   storageBucket: process.env.REACT_APP_STORAGEBUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
   appId: process.env.REACT_APP_APPID
